feae89838c2bcd3187a1ce3cd98b8491dca79b82
Mark <mal@luigiMac.local>
2014-05-17 17:54:08 -0400

    Initial Commit

---
+++ test1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test1
+//
+//  Created by Mark on 5/17/14.
+//  Copyright (c) 2014 Mark. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
